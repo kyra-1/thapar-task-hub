154567fce9ffa0e7e6d39a9a6faa9cb14c425ba6
Yashika Aggarwal <151565236+kyra-1@users.noreply.github.com>
2025-09-25 18:31:53 +0530

    Update App.tsx to include profile

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ const App = () => (
             <Route path="/" element={<Index />} />
             <Route path="/auth" element={<Auth />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/profile/:userId" element={<Profile />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
